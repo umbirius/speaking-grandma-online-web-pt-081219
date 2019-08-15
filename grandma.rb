8e337fa122130e668d4a66f5a984936718888fd3
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-15 20:22:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,9 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma (phrase)
+  if phrase != phrase.upcase
+    puts 
+    "I LOVE YOU GRANDMA!"
+    
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
