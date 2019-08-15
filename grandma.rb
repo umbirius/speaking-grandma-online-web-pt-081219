651957dcc556f5fb66eef1905726753ed27924c0
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-15 20:25:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma (phrase)
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elseif phrase != phrase.upcase && phrase != "I LOVE YOU GRANDMA"
+  elsif phrase != phrase.upcase && phrase != "I LOVE YOU GRANDMA"
     puts "NOT SINCE 1938!"
-  elseif phrase == "I LOVE YOU GRANDMA"
+  elsif phrase == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end 
